1ccc8217bfcad8e5070b31c270a01ee84bcb62cf
2bit <github@2bit.jp>
2026-08-12 18:52:31 +0900

    Fix deterministic editor paint sampling

--- Tests/EditorTests.cpp
+++ Tests/EditorTests.cpp
@@ -28,9 +28,11 @@ void checkPaintContract(juce::AudioProcessorEditor& editor)
 {
     juce::Image image(juce::Image::RGB, BrickMawAudioProcessorEditor::defaultWidth,
                       BrickMawAudioProcessorEditor::defaultHeight, true);
-    juce::Graphics g(image);
     editor.setBounds(0, 0, image.getWidth(), image.getHeight());
-    editor.paint(g);
+    {
+        juce::Graphics g(image);
+        editor.paint(g);
+    }
 
     const auto background = ehl::juce_design::Palette::ink();
     const auto divider = ehl::juce_design::Palette::low();
